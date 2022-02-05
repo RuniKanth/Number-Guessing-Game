b4bad1e94f544791bb0cf8715c91ca44dbd939d9
Runi Kanth <98279835+RuniKanth@users.noreply.github.com>
2022-02-05 14:13:32 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 'use strict';
 
-document.querySelector('.message').textContent = '🧡 You beat me!';
+//Listen to the event on the check button by selecting it
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = document.querySelector('.guess').value;
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🖥🔒Please input a number';
+  }
+});
